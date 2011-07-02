75d553b0db0a59a7d818ad6c712bb5a6aaee3c98
Richard W. Norton <rwtnorton@gmail.com>
2011-07-02 14:58:28 -0400

    Added Panel and Chunker.

--- bacon.rb
+++ bacon.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'chunky_png'
+require 'pp'
 
 # 300 x 225
 image = ChunkyPNG::Image.from_file('output.png')
@@ -14,6 +15,10 @@ module Chunks
   def self.ensure_length(total, start, len)
     start + len > total ? total - start : len
   end
+
+  # Returns Array of indexes, treating +self+ as a +width+ by +height+
+  # matrix (origin at upper-left corner), where returned indexes match
+  # the box (chunk) at (+x+, +y+) of width +w+ and height +h+.
   def self.calculate_chunked_indexes(width, height, x, y, w, h)
     w = ensure_length(width,  x, w)
     h = ensure_length(height, y, h)
@@ -25,6 +30,48 @@ module Chunks
     end
     chunks
   end
+
+  class Panel
+    attr_reader :x, :y, :w, :h
+    def initialize(x, y, w, h)
+      @x, @y, @w, @h = x, y, w, h
+    end
+    def inspect
+      x = sprintf '%3d', @x
+      y = sprintf '%3d', @y
+      "[#{x},#{y}]"
+    end
+  end
+
+  # Helper class for partitioning an area into squares (panels).
+  # +panels+ is the payoff, containing an array of Panels.
+  # If +image_width+ or +image_height+ are not evenly divisible by
+  # +panel_size+, then extra panels will be added that overlap other panels.
+  class Chunker
+    attr_reader :panels, :panel_size, :image_width, :image_height
+    def initialize(image_width, image_height, panel_size=10)
+      @image_width, @image_height, @panel_size =
+       image_width,  image_height,  panel_size
+
+      col_count, extra_col = image_width.divmod(panel_size)
+      row_count, extra_row = image_height.divmod(panel_size)
+      extra_col = extra_col > 0
+      extra_row = extra_row > 0
+
+      grid_xs = (0 .. col_count - 1).map {|x| x * panel_size }
+      grid_xs << image_width - panel_size if extra_col
+      grid_ys = (0 .. row_count - 1).map {|y| y * panel_size }
+      grid_ys << image_height - panel_size if extra_row
+
+      panels = []
+      grid_ys.each do |y|
+        grid_xs.each do |x|
+          panels << Panel.new(x, y, panel_size, panel_size)
+        end
+      end
+      @panels = panels
+    end
+  end
 end
 
 class Array
@@ -35,18 +82,6 @@ class Array
 end
 
 class ChunkyPNG::Canvas
-
-#  def extract_chunk(x, y, w, h)
-#    w = width  - x if x + w > width
-#    h = height - y if y + h > height
-#    new_pixels = []
-#    y.upto(y + h - 1) do |row|
-#      col_start = row * width + x
-#      col_end   = row * width + x + w - 1
-#      new_pixels += self.pixels[col_start .. col_end]
-#    end
-#    self.class.new(w, h, new_pixels)
-#  end
   def extract_chunk(x, y, w, h)
     w = Chunks.ensure_length(width,  x, w)
     h = Chunks.ensure_length(height, y, h)
@@ -56,23 +91,9 @@ class ChunkyPNG::Canvas
 
 end
 
-# Try to split the image into two vertically.
-left_image   = image.extract_chunk(  0,   0,
-                                   image.width / 2, image.height)
-right_image  = image.extract_chunk(image.width / 2, 0,
-                                   image.width / 2, image.height)
-left_image.save('images/left.png')
-right_image.save('images/right.png')
-
-# and now horizontally.
-top_image    = image.extract_chunk(  0,   0,
-                                   image.width, image.height / 2)
-bottom_image = image.extract_chunk(  0, image.height / 2,
-                                   image.width, image.height / 2 + 1)
-top_image.save('images/top.png')
-bottom_image.save('images/bottom.png')
-
-__END__
-0.upto(300 / 10) do |x|
-  0.upto(
+chunker = Chunks::Chunker.new(image.width, image.height, 50)
+pp chunker.panels
+chunker.panels.each do |panel|
+  chunk = image.extract_chunk(panel.x, panel.y, panel.w, panel.h)
+  chunk.save("images/chunk-#{panel.x}-#{panel.y}.png")
 end
